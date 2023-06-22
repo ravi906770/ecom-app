6d24487d3a080a5026607c8a281fdd59e30ded45
Ravi <pankhaniyaravi05@gmail.com>
2023-06-22 14:09:07 +0530

    final code
    ecommarce app

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan';
+import connection from './config/db.js';
+import authRoutes from './routes/authroute.js';
+import categoryRoutes from "./routes/categoryRoutes.js"
+import productRoutes from "./routes/productRoutes.js"
+import cors from "cors"
+import path from "path"
+
+dotenv.config();
+
+connection();
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+app.use('/api/v1/auth' , authRoutes)
+app.use('/api/v1/category' , categoryRoutes)
+// app.use('api/v1/product' , productRoutes)
+app.use("/api/v1/product", productRoutes);
+app.use(express.static(path.join(__dirname , "./client/build")))
+
+
+app.use('*' , function(req,res){
+    res.sendFile(path.join(__dirname , './client/build/index.html'))
+})
+
+//PORT
+
+const PORT = process.env.PORT;
+
+app.listen(PORT , ()=>{
+    console.log(`server will running on ${PORT}`)
+})
